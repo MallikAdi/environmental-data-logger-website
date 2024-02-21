02877f3338ddfbed97e09a40b79b0e2c5c6517d5
ruhiii19 <ruhii.panjwani19@gmail.com>
2024-02-21 23:46:15 +0530

    moved icons to home screen instead of App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import HomeScreen from "./screens/HomeScreen";
-import { IoRainySharp } from "react-icons/io5";
-import { TbDroplet } from "react-icons/tb";
-import { MdOutlineLineStyle } from "react-icons/md";
-import { FaTemperatureEmpty } from "react-icons/fa6";
 
 function App() {
   const [data, setData] = useState<any>(null);
@@ -34,24 +30,7 @@ function App() {
   return (
     <div className="App">
       <div className="lower-layer">
-        <div className="strip">
-          <span className="icon">
-            <FaTemperatureEmpty />
-          </span>
-          <span className="icon">
-            <IoRainySharp />
-          </span>
-          <span className="icon">
-            <TbDroplet />
-          </span>
-          <span className="icon">
-            <MdOutlineLineStyle />
-          </span>
-        </div>
-        <div className="main-content">
-          {/*data ? <HomeScreen data={data} /> : <p>Loading...</p>*/}
-          <HomeScreen data={data} />
-        </div>
+        <HomeScreen data={data} />
       </div>
     </div>
   );
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
-import Sidebar from '../components/Sidebar'
-import Graph from '../components/Graph'
-import '../screens/HomeScreenStyles.css'
+import React from "react";
+import Sidebar from "../components/Sidebar";
+import Graph from "../components/Graph";
+import "../screens/HomeScreenStyles.css";
 
-interface Props{
+import { IoRainySharp } from "react-icons/io5";
+import { TbDroplet } from "react-icons/tb";
+import { MdOutlineLineStyle } from "react-icons/md";
+import { FaTemperatureEmpty } from "react-icons/fa6";
+
+interface Props {
   data: {
     Batt: number;
     Temp: number;
@@ -14,17 +19,36 @@ interface Props{
   }[];
 }
 
-const HomeScreen:React.FC<Props> = ({data}) => {
-  console.log('Fetched Data:', data);
-  const temperatureData = data ? data.map(item => item.Temp) : [];
+const HomeScreen: React.FC<Props> = ({ data }) => {
+  console.log("Fetched Data:", data);
+  const temperatureData = data ? data.map((item) => item.Temp) : [];
   return (
     <div className="homeScreenContainer">
-      <div className="sidebar"><Sidebar temperatureData={temperatureData}/></div>
-      <div className="graph" >
-        <Graph temperatureData={temperatureData}/>
+      <div className="lower-layer">
+        <div className="strip">
+          <span className="icon">
+            <FaTemperatureEmpty />
+          </span>
+          <span className="icon">
+            <IoRainySharp />
+          </span>
+          <span className="icon">
+            <TbDroplet />
+          </span>
+          <span className="icon">
+            <MdOutlineLineStyle />
+          </span>
+        </div>
+
+        <div className="sidebar">
+          <Sidebar temperatureData={temperatureData} />
+        </div>
+        <div className="graph">
+          <Graph temperatureData={temperatureData} />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
